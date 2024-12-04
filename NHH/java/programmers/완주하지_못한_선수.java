c7ee5a6e908b0696842402880a2c935b4fd0bad3
nohyuha <nohyunha95@gmail.com>
2024-12-04 23:40:59 +0900

    프로그래머스: 해시 문제 풀이

---
+++ NHH/java/programmers/완주하지_못한_선수.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.*;
+
+public class 완주하지_못한_선수 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        System.out.println(solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        System.out.println(solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+
+        String answer = "";
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                answer = entry.getKey();
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ NHH/java/programmers/의상.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+        System.out.println(solution(new String[][]{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}}));
+    }
+
+    public static int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            if (map.containsKey(clothe[1])) {
+                map.put(clothe[1], map.get(clothe[1]) + 1);
+            } else {
+                map.put(clothe[1], 1);
+            }
+        }
+
+        // 경우의 수를 수식으로 작성한다.
+        // 기본적으로 N x M이 경우의 수이다.
+        // 안입는 경우를 +1로 계산한다. (N+1) x (M+1)
+        // 모두 안입는 경우는 없으므로, -1 해줘야 조건에 만족하는 경우의 수를 구할 수 있다
+        // 수식 = (N+1) x (M+1) - 1
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        return answer - 1;
+    }
+}
